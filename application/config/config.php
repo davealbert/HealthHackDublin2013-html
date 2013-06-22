3703934be8b5b9dee75f5fadd9ec1c248f1ddc31
Dave Albert <dave.albert@gmail.com>
2013-06-22 04:29:48 +0100

    #HealthHackDublin2013 Configuration setup

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'sa7s7ashd77sa77as7';
 
 /*
 |--------------------------------------------------------------------------
