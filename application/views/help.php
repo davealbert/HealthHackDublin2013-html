73fe6b4e3306a92367ed27890942a01a54c7116c
Dave Albert <dave.albert@gmail.com>
2013-06-22 18:48:31 +0100

    Help controller and view used for introduction.

---
+++ application/views/help.php
@@ -0,0 +1,11 @@
+
+<div class="hero-unit">
+	<h1>Touch & Talk</h1>
+	<p><i>Patient's voices heard</i></p>
+	<p>Enabling communication between patients, families and caregivers.</p>
+	<p>
+		<a class="btn btn-primary btn-large">
+			START
+		</a>
+	</p>
+</div>
