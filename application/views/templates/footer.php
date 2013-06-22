af4c872c61432cb893520f771c1acca61f766355
Goldgram <willbrennandesign@gmail.com>
2013-06-22 16:44:37 +0100

    added awesome fonts and seperated header and footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,18 @@
-
-</body>
+		
+		<div id="wrapper">
+			<div id="pageNav">
+				<div id="pageNavLeft">
+					<a href=""><div class="navButton1"><i class="icon-home icon-3x" style="float:left;"></i><p>HOME</p></div></a>
+					<a href=""><div class="navButton1"><i class="icon-calendar icon-3x" style="float:left;"></i><p>CALLENDAR</p></div></a>
+					<a href=""><div class="navButton1"><i class="icon-arrow-left icon-3x" style="float:left;"></i><p>BACK</p></div></a>
+					<a href=""><div class="navButton1"><i class="icon-keyboard icon-3x" style="float:left;"></i><p>KEYPAD</p></div></a>
+					<div style="clear:both;"></div>
+				</div>
+				<!-- <div class="navButton2">HELP</div> -->
+				<a href=""><div class="navButton2"><i class="icon-exclamation icon-3x" style="float:left;"></i><p>HELP</p></div></a>
+					
+				<div style="clear:both;"></div>
+			</div>
+		</div>
+	</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,36 +7,18 @@
 	<link rel="stylesheet" type='text/css' media='all' href="/css/bootstrap.min.css"/>
 	<link rel="stylesheet" type='text/css' media='all' href="/css/bootstrap-responsive.css"/>
 	<link rel="stylesheet" type='text/css' media='all' href="/css/main.css"/>
+	<link rel="stylesheet" href="/css/font-awesome.min.css">
+
+
+
+
 	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<!-- <div class="navbar navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-				<a class="brand" href="/">Project Name</a>
-				<div class="nav-collapse">
-					<ul class="nav">
-						<li class="active"><a href="/"><i class="icon-home icon-white"></i> Home</a></li>
-						<li><a href="/api/data">Data</a></li>
-						<li><a href="#">Link</a></li>
-						<li><a href="#">Link</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Action</a></li>
-								<li><a href="#">Another action</a></li>
-							</ul>
-						</li>
-					</ul>
-					<form class="navbar-search pull-right" action="/search">
-						<input type="text" class="search-query span2" placeholder="Search" id="q" name="q">
-					</form>
-				</div>
-			</div>
+	<div id="wrapper">
+		<div id="pageHeadtext">
+			<p>Text 1 dsbfhhbskfjn debesjhbf fygbsjubf fysebf</p>
+			<p>Text 2 hebfkj dfcdsjb fhbdsbf</p>
+			<p style="font-size:20px;">Text 3 hfbd fhbdbbibfkds fbdkbf fu df</p>
 		</div>
-</div>-->
+	</div>
