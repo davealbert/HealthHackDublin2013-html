bef81babf5ec028b161d95f0577c586e5c39cc1b
Goldgram <willbrennandesign@gmail.com>
2013-06-22 17:15:34 +0100

    finished footer buttons

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,12 +5,10 @@
 					<a href=""><div class="navButton1"><i class="icon-home icon-3x" style="float:left;"></i><p>HOME</p></div></a>
 					<a href=""><div class="navButton1"><i class="icon-calendar icon-3x" style="float:left;"></i><p>CALLENDAR</p></div></a>
 					<a href=""><div class="navButton1"><i class="icon-arrow-left icon-3x" style="float:left;"></i><p>BACK</p></div></a>
-					<a href=""><div class="navButton1"><i class="icon-keyboard icon-3x" style="float:left;"></i><p>KEYPAD</p></div></a>
+					<a href=""><div class="navButton1"><i class="icon-keyboard icon-3x" style="float:left;"></i><p>KEYBOARD</p></div></a>
 					<div style="clear:both;"></div>
 				</div>
-				<!-- <div class="navButton2">HELP</div> -->
-				<a href=""><div class="navButton2"><p style="float:left;">HELP</p><i class="icon-exclamation icon-4x"></i></div></a>
-					
+				<a href=""><div class="navButton2"><p style="float:left;">HELP<span style="margin-left:10px;"></span><i class="icon-exclamation-sign"></i></p></div></a>
 				<div style="clear:both;"></div>
 			</div>
 		</div>
